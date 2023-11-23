ff8e7cad540922f999e300d3ebd11e2c048695a2
Serhii <135651866+opsmanager1@users.noreply.github.com>
2023-11-23 02:43:17 +0200

    Update Navbar.tsx

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -24,8 +24,8 @@ export function Navbar() {
           </Link>
           <div className={styles.navMiddle}>
             <Link href="/buy">
-            <a class="sliding-button1">Buy</a>
-            </Link>       
+            <a className="sliding-button1">Buy</a>
+            </Link>      
             <Link className={styles.heroCtasell} href="/sell">
               Sell
             </Link>
