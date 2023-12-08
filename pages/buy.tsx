84a60a380572729d2a86e4848be2cb0426dee393
Serhii <135651866+opsmanager1@users.noreply.github.com>
2023-12-08 18:47:42 +0200

    Update buy.tsx

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -3,11 +3,6 @@ import React from "react";
 import Container from "../components/Container/Container";
 import NFTGrid from "../components/NFT/NFTGrid";
 import { NFT_COLLECTION_ADDRESS } from "../const/contractAddresses";
-import styles from "../styles/Home.module.css";
-import type { NextPage } from "next";
-import Link from "next/link";
-import Image from "next/image";
-import backgroundImage from '../public/DYMBACK.png';
 
 export default function Buy() {
   // Load all of the NFTs from the NFT Collection
@@ -15,28 +10,7 @@ export default function Buy() {
   const { data, isLoading } = useNFTs(contract);
 
   return (
-    <div className={styles.backgroundContainer}>
-     <div className={styles.contentContainer}>
-        <Image
-          src={backgroundImage}
-          alt="Background Image"
-          layout="fill"
-          objectFit="cover"
-        />
-   <div className={styles.content}>
-   <div className={styles.hero}>
-     <div className={styles.heroBackground}>
-      <div className={styles.heroBackgroundInner}>
-              <Image
-                src="/hero-gradient.png"
-                width={1390}
-                height={1390}
-                alt="Background gradient from red to blue"
-                quality={100}
-                className={styles.gradient}
-              />
-            </div>
-          </div>
+    <Container maxWidth="lg">
       <h1>Buy NFTs</h1>
       <p>Browse which NFTs are available from the collection.</p>
       <NFTGrid
@@ -46,8 +20,6 @@ export default function Buy() {
           "Looks like there are no NFTs in this collection. Did you import your contract on the thirdweb dashboard? https://thirdweb.com/dashboard"
         }
       />
-      </div>
-    </div>
-   </div>
+    </Container>
   );
 }
