cda2e465ad1188133fbdf1797ab4eed57da9b3b3
Serhii <135651866+opsmanager1@users.noreply.github.com>
2023-12-09 10:52:14 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,8 +53,8 @@ const Home: NextPage = () => {
             <Image
               style={{ marginTop: 120 }}
               src="/The future is modular(1).gif"
-              width={860}
-              height={540}
+              width={1250}
+              height={220}
               alt="Hero asset, NFT marketplace"
               quality={100}
               className={styles.heroAsset}
